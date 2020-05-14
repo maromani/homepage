82048c83cc80e9da3a4985793b11af37dcec0764
Marcin Romaniuk <romaniukma@gmail.com>
2020-05-14 20:40:05 +0200

    Implementation BEM

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-console.log ('Cześć tu Roman');
-const napis1 = 'Cześć tu Roman'
-const liczba1 = 22742
+
+const napis1 = "Cześć tu Roman";
+let liczba1 = 22742;
+liczba1 = 2274;
 
 //alert ('lala1');
 //alert (liczba1);
-console.log(`Szczęsliwa liczba to ${liczba1}`); 
+console.log(`Szczęsliwa liczba Romana to ${liczba1}`);
+const article_main = document.querySelector('.article__main--js');
+console.log(article_main.innerHTML);
+article_main.innerHTML = `${article_main.innerHTML} + dodatkowy tekst dodany przy pomocy JS`;
+
+const article = document.querySelector('.article__description--link--js');
+console.log(article.innerHTML);
+article.innerHTML = `${article.innerHTML} + dodatkowy tekst dodany przy pomocy JS`;
